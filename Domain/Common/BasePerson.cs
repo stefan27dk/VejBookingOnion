b5b9485fd28c4adb3c30918c9881d6866995b819
Stefan Biserov Popov <stef9633@edu.ucl.dk>
2020-10-14 19:45:12 +0200

    Add project files.

---
+++ Domain/Common/BasePerson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Common
+{
+    public class BasePerson : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }      
+    }
+}
